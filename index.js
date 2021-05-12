d09e05caa01f0d986b0fef955db4c12fab0a2ea9
Divyalok Jaiswal <divyjais2001@gmail.com>
2021-05-12 11:28:55 +0530

    finished ejs layouts

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const port = 2021;
 const app = express();
-
 const db = require('./config/mongoose');
+const expressLayouts = require('express-ejs-layouts');
 
-app.set('view engine', 'ejs');
+//static files
 app.use(express.static(__dirname + '/assets'));
 
-app.get('/', function(req, res) {
-    res.render('main');
-});
+//to extract scripts && styles 
+app.use(expressLayouts);
+app.set('layout extractScripts', true);
+app.set('layout extractStyles', true);
+app.set('layout home', false);
+
+//setting the view engine
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/index'));
+
 
 app.listen(port, function(err){
     if(err) {
         console.log(`Error encountered: ${err}`);
+    } else {
+        console.log(`Server is running at port: ${port}`);
     }
 });
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('main');
+});
+
+router.get('/home', function(req, res) {
+    res.render('home', {
+        layout: 'homelayout'
+    });
+});
+
+module.exports = router;
